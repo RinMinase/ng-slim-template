09852a50edf3d15700f49a7b287423a8e4d7f7a0
Rin Minase <rin.black.raison.detre@gmail.com>
2020-06-28 11:59:30 +0800

    Added example navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <div style="text-align:center" class="content">
-      <h1>Welcome!</h1>
-    </div>
-    <router-outlet></router-outlet>
-  `,
-  styles: []
+	selector: 'app-root',
+	template: `
+		<div style="text-align:center" class="content">
+			<h1>Navigation</h1>
+			<a class="menu-padding" routerLink="/">Landing</a>
+			<a class="menu-padding" routerLink="/home">Home</a>
+			<a class="menu-padding" routerLink="/about">About</a>
+		</div>
+		<router-outlet></router-outlet>
+	`,
+	styles: []
 })
 export class AppComponent {}
